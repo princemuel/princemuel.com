98024f35e0654fe251719631cecd481eb85f56d7
Prince Muel <vansomecsam@gmail.com>
2023-09-20 13:51:28 +0100

    feat: init pages
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      Hello
-    </main>
-  );
+  return <main className='flex min-h-screen flex-col'>Home Page</main>;
 }
