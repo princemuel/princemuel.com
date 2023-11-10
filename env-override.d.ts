59b411cc4d4aadb0d404b0bc2d55f1c07649efec
Prince Muel <vansomecsam@gmail.com>
2023-11-10 07:44:21 +0100

    chore: migrate server from vite to remix
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- env-override.d.ts
+++ env-override.d.ts
@@ -1,13 +1,3 @@
-interface ImportMetaEnv {
-  readonly VITE_SERVER_URL: string;
-  readonly SESSION_TOKEN: string;
-  // more env variables...
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
-
 declare module '*.mdx' {
   let MDXComponent: (props: any) => JSX.Element;
   export const frontmatter: any;
--- env.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="@remix-run/node" />
-/// <reference types="vite-plugin-svgr/client" />
-/// <reference types="./env-override.d.ts" />
-/// <reference types="vite/client" />
