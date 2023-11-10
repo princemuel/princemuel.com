012909738daca8a1ac44858e2addb61418133e06
Prince Muel <vansomecsam@gmail.com>
2023-11-10 08:09:49 +0100

    feat: setup css styles
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,10 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 export default {
+  darkMode: ['class'],
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
-} satisfies Config
+  plugins: [require('tailwindcss-animate')],
+} satisfies Config;
