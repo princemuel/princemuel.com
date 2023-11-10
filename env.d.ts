ee03083321154668a8f666a72c389da7c4a53983
Prince Muel <vansomecsam@gmail.com>
2023-11-10 05:39:59 +0100

    fix: update project types
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- env.d.ts
+++ env.d.ts
@@ -1,17 +1,4 @@
 /// <reference types="@remix-run/node" />
+/// <reference types="vite-plugin-svgr/client" />
+/// <reference types="./env-override.d.ts" />
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_SERVER_URL: string;
-  // more env variables...
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
-
-declare module '*.mdx' {
-  let MDXComponent: (props: any) => JSX.Element;
-  export const frontmatter: any;
-  export default MDXComponent;
-}
