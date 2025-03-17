a22dfeae25ee6aba4e6edbd20b2e56f4835ac0c7
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-03-17 11:09:43 +0100

    ci(reporting): enable Spotlightjs in Sentry

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,6 +1,8 @@
 import * as Sentry from "@sentry/astro";
+import * as Spotlight from "@spotlightjs/spotlight";
 
 Sentry.init({
   dsn: "https://6b6896649eefeae398f66bd56d9d879e@o4505133438533632.ingest.us.sentry.io/4508977534009344",
   integrations: [],
 });
+Spotlight.init();
